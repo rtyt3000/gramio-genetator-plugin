ed295292f066f725fdbce1c16c520c6cd6e1a62c
GitHub Action <action@github.com>
2025-04-27 21:34:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/rtyt3000/gramiogenetatorplugin/MyBundle.kt
+++ src/main/kotlin/com/github/rtyt3000/gramiogenetatorplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.rtyt3000.gramiogenetatorplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/rtyt3000/gramiogenetatorplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/rtyt3000/gramiogenetatorplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.rtyt3000.gramiogenetatorplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
